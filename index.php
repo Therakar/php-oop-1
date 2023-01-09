3eb72e000156947cf1ef59deb3d542759b03976a
Therakar <matteo.pau200@gmail.com>
2023-01-09 20:01:47 +0100

    crea la classe

--- index.php
+++ index.php
@@ -1,21 +1,10 @@
 <?php
 
-
+// classe
+class Movie{
+    
 }
 
-$film1 = new Movie("Harry Potter", "Fantasy", "2:49", 2001, 7);
-echo $film1->title;
-echo $film1->genre;
-echo $film1->duration;
-echo $film1->year;
-echo $film1->vote;
-
-$film2 = new Movie("Il signore degli anelli", "Fantasy", "3:00", 2001, 3);
-echo $film2->title;
-echo $film2->genre;
-echo $film2->duration;
-echo $film2->year;
-echo $film2->vote;
 
 ?>
 
