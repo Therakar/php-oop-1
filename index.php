0ceedc51bc293ffadc4da535b8f8e3b2f3875b41
Therakar <matteo.pau200@gmail.com>
2023-01-09 20:00:33 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+
+}
+
+$film1 = new Movie("Harry Potter", "Fantasy", "2:49", 2001, 7);
+echo $film1->title;
+echo $film1->genre;
+echo $film1->duration;
+echo $film1->year;
+echo $film1->vote;
+
+$film2 = new Movie("Il signore degli anelli", "Fantasy", "3:00", 2001, 3);
+echo $film2->title;
+echo $film2->genre;
+echo $film2->duration;
+echo $film2->year;
+echo $film2->vote;
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>oop exercise</title>
+</head>
+<body>
+  
+</body>
+</html>
